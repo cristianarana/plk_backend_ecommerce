c676de9c2824188e3a33b2316ce99375d9000430
Cristian Arana <cristianarana767@gmail.com>
2026-08-06 19:34:03 -0300

    fix docker and add docker-compose override

--- src/config/database/datasource.ts
+++ src/config/database/datasource.ts
@@ -9,5 +9,5 @@ export const AppDataSource = new DataSource({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   entities: [],
-  synchronize: true,
+  synchronize: process.env.NODE_ENV === 'dev',
 });
